c47a528d805ba8da512438c419567c46197e560b
SHIBAURA\H0882 <t_iwasaki@shibaura-e.co.jp>
2020-11-30 12:57:43 +0900

    HTTPクライアントのエラーハンドリング追加

--- src/app/service/in-memory-data.service.ts
+++ src/app/service/in-memory-data.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Member } from '../interface/member';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+  
+  createDb() {
+    const members = [
+      {id:1, name: '高橋 和彦'},
+      {id:2, name: '白取 直弘'},
+      {id:3, name: '畠山 光'　},
+      {id:4, name: '伊藤 百美'},
+      {id:5, name: '岩崎 利己'},
+      {id:6, name: '白鳥 昂太'}
+    ];
 
-  constructor() { }
+    return { members };
+  }
+
+  genId(members: Member[]): number {
+    return members.length > 0 ? Math.max(...members.map(member => member.id)) + 1 : 5;
+  } 
 }
--- src/app/service/member.service.ts
+++ src/app/service/member.service.ts
@@ -1,23 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from '../interface/member';
 import { MEMBERS } from '../interface/mock-members';
 import { MessageService } from './message.service';
+import { catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root' //rootの場合アプリケーション全体で使用できるサービス
 })
 export class MemberService {
 
-  constructor(private messageService: MessageService) { }
+  private membersURL = 'api/members';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+    ) { }
 
   getMembers(): Observable<Member[]>{
-    this.messageService.add('MemberService: 社員一覧データを取得しました');
-    return of(MEMBERS);
-  }
+    return this.http.get<Member[]>(this.membersURL)
+      .pipe(
+        tap(members => this.log('社員データを取得しました')), //中間処理で実行するメソッド
+        catchError(this.handleError<Member[]>('getMembers',[]))
+      );
+    }
 
   getMember(id: number): Observable<Member> {
-    this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
-    return of(MEMBERS.find(member => member.id === id));
+    const URL = `${this.membersURL}/${id}`;
+    return this.http.get<Member>(URL)
+      .pipe(
+        tap( _ => this.log(`社員データ(id=${id})を取得しました`)),
+        catchError(this.handleError<Member>(`getMember id = ${id}`))
+      );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`MemberService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation',result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} 失敗:${error.message}`);
+
+      return of(result as T);
+    }
   }
 }
